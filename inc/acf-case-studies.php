d755c716b74a9c19dbf0f5cb81f5485187c67b49
timothyjturner <timothyjturner@gmail.com>
2026-02-25 14:25:42 -0500

    removed case study editor toggle

--- inc/acf-case-studies.php
+++ inc/acf-case-studies.php
@@ -8,19 +8,3 @@ add_filter('acf/settings/load_json', function ($paths) {
   $paths[] = get_template_directory() . '/acf-json';
   return $paths;
 });
-
-add_action('admin_enqueue_scripts', function ($hook) {
-    // Only on post editor screens
-    if (!in_array($hook, ['post.php', 'post-new.php'], true)) return;
-  
-    $screen = function_exists('get_current_screen') ? get_current_screen() : null;
-    if (!$screen || $screen->post_type !== 'case_study') return;
-  
-    wp_enqueue_script(
-      'jmdc-case-study-editor-toggle',
-      get_template_directory_uri() . '/assets/js/case-study-editor-toggle.js',
-      ['jquery'],
-      '1.0.0',
-      true
-    );
-  });
